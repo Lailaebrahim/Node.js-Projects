4b7ab056282306c391860dae6658ba81a748d4e2
Lailaebrahim <ebrahimlaila815@gmail.com>
2025-08-17 13:07:53 +0300

    add manual file handling to laptop CRUD operations

--- LAPS/src/controllers/laptop.controller.ts
+++ LAPS/src/controllers/laptop.controller.ts
@@ -43,7 +43,31 @@ class LaptopController {
 
   createLaptop = expressAsyncHandler(
     async (req: Request, res: Response, next: NextFunction) => {
-      const laptop = await Laptop.create(req.body);
+      const laptop = await Laptop.create({
+        model_name: req.body.model_name,
+        brand: req.body.brand,
+        processor_name: req.body.processor_name,
+        ram: req.body.ram,
+        ssd: req.body.ssd,
+        hard_disk: req.body.hard_disk,
+        operating_system: req.body.operating_system,
+        graphics: req.body.graphics,
+        screen_size_inches: req.body.screen_size_inches,
+        resolution_pixels: req.body.resolution_pixels,
+        no_of_cores: req.body.no_of_cores,
+        no_of_threads: req.body.no_of_threads,
+        spec_score: req.body.spec_score,
+        price: req.body.price,
+      });
+
+      if(req.file?.filename){
+        laptop.manual ={
+          fileName: String(req.file?.filename),
+          vectorized: false,
+        }
+        laptop.save();
+      }
+
       res.status(201).json({
         status: "success",
         data: laptop,
@@ -53,13 +77,73 @@ class LaptopController {
 
   updateLaptop = expressAsyncHandler(
     async (req: Request, res: Response, next: NextFunction) => {
-      const laptop = await Laptop.findByIdAndUpdate(req.params.id, req.body, {
-        new: true,
-        runValidators: true,
+      const allowedFields: Partial<{
+        model_name: string;
+        brand: string;
+        processor_name: string;
+        ram: number;
+        ssd: number;
+        hard_disk: number;
+        operating_system: string;
+        graphics: string;
+        screen_size_inches: number;
+        resolution_pixels: string;
+        no_of_cores: number;
+        no_of_threads: number;
+        spec_score: number;
+        price: number;
+      }> = {};
+
+      const allowedFieldNames = [
+        "model_name",
+        "brand",
+        "processor_name",
+        "ram",
+        "ssd",
+        "hard_disk",
+        "operating_system",
+        "graphics",
+        "screen_size_inches",
+        "resolution_pixels",
+        "no_of_cores",
+        "no_of_threads",
+        "spec_score",
+        "price",
+      ];
+
+      allowedFieldNames.forEach((field) => {
+        if (req.body[field] !== undefined) {
+          allowedFields[field as keyof typeof allowedFields] = req.body[field];
+        }
       });
+
+      const laptop = (await Laptop.findByIdAndUpdate(
+        req.params.id,
+        allowedFields,
+        {
+          new: true,
+          runValidators: true,
+        }
+      )) as any;
+
+      // there is no overlapping in accessing laptop doc and adding both deletion job and file processing job (by calling save method on document)
+      // as the worker process don't access or change anython in the database
+      // only the file processing job changed the laptop.manual.vectorized filed to true after adding the vectorized doc to pincone index successfully
+      if (req.file?.filename && laptop) {
+        if (laptop.manual.fileName && laptop.manual.fileName !== req.file.filename) {
+          await laptop.triggerFileDeletion();
+        }
+        laptop.manual = {
+          fileName: String(req.file.filename),
+          vectorized: false,
+        };
+        await laptop.save();
+      }
+
       if (!laptop) {
         return next(new AppError("No Laptop found with that ID", 404));
       }
+
       res.status(200).json({
         status: "success",
         data: laptop,
@@ -69,8 +153,8 @@ class LaptopController {
 
   deleteLaptop = expressAsyncHandler(
     async (req: Request, res: Response, next: NextFunction) => {
-      const laptop = await Laptop.findByIdAndDelete(req.params.id);
-      if (!laptop) {
+      const laptop = await Laptop.deleteOne({ _id: req.params.id });
+      if (laptop.deletedCount == 0) {
         return next(new AppError("No Laptop found with that ID", 404));
       }
       res.status(200).json({
@@ -126,6 +210,7 @@ class LaptopController {
       });
     }
   );
+  
 }
 
 const laptopController = new LaptopController();
